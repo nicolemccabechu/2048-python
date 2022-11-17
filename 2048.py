a576700593adacac3ff50a312b87464922fa8916
Nicole <nicolemccabechu@gmail.com>
2022-11-17 20:26:08 +0000

    [ui] Create grid GUI and score header

---
+++ 2048.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import random
+import colors as c
+
+
+class Game(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.master.title('2048')
+
+        self.main_grid = tk.Frame(
+            self, bg=c.GRID_COLOR, bd=3, width=400, height=400)
+        self.main_grid.grid(pady=(80, 0))
+        self.make_GUI()
+
+        self.mainloop()
+
+
+    def make_GUI(self):
+        self.cells = []
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    bg=c.EMPTY_CELL_COLOR,
+                    width=100,
+                    height=100)
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+
+        score_frame = tk.Frame(self)
+        score_frame.place(relx=0.5, y=40, anchor="center")
+        tk.Label(
+            score_frame,
+            text="Score",
+            font=c.SCORE_LABEL_FONT).grid(
+            row=0)
+        self.score_label = tk.Label(score_frame, text="0", font=c.SCORE_FONT)
+        self.score_label.grid(row=1)
+
+
+    
